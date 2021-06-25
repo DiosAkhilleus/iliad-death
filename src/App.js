e70d8234ca74703caf69adf548220ef767384694
DiosAkhilleus <abertin1809@gmail.com>
2021-06-24 21:52:58 -0500

    reconfigured Linechart and death slider to be part of same component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 // import {getCount} from './javascript/deaths';
 import { useState, useEffect } from 'react';
 import Linechart from './Components/Linechart';
-import Slider, {Range} from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import './App.css';
 
 function App() {
 
-  const [selectedBooks, setSelectedBooks] = useState([0, 24]);
-  const [sliderValues, setSliderValues] = useState([1, 24]);
-  const setChartRange = () => {
-    setSelectedBooks([4, 15]);
-  }
+  
 
-  useEffect(() => {
-    setSelectedBooks(sliderValues);
-  }, [sliderValues])
+  // useEffect(() => {
+  //   setSelectedBooks(sliderValues);
+  // }, [sliderValues])
 
-  const handleSliderChange = (sliderValues) => {setSliderValues(sliderValues)}
 
   const format = {
     Assailant: "Name",
@@ -34,14 +28,8 @@ function App() {
 
   return (
     <div className="display">
-      <h1 className="title">An interactive display of death in the Iliad</h1>
-      <button onClick={setChartRange}>Click Me</button>
-      <Range className="book-range" min={1} max={24} defaultValue={sliderValues} marks={{24: {label: "24"}, 12: {label: "12"}, 1: {label: "1"}}} onChange={handleSliderChange} />
-      <div className='chart-container'>
-        {/* {getCount("Wound_Location", "Back")} */}
-        <Linechart className='death-chart' selectedBooks={selectedBooks}/>
+        <Linechart className='death-chart'/>
       </div>
-    </div>
   );
 }
 
--- src/Components/Linechart.js
+++ src/Components/Linechart.js
@@ -1,18 +1,47 @@
 import React from 'react'
 import {useState, useEffect} from 'react';
-import { LineChart, Line, XAxis, YAxis } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip} from 'recharts';
+import Slider, {Range} from 'rc-slider';
+import '../App.css';
+
 // const attackData = require('../json/Iliad_Deaths.json').Attacks;
 
-const Linechart = ( {selectedBooks} ) => {
+const Linechart = () => {
  
-  const [dataArr, setDataArr] = useState ([{"Book": "1", "Death_Total": 0}, {"Book": "2", "Death_Total": 0}, {"Book": "3", "Death_Total": 0}, {"Book": "4", "Death_Total": 7}, {"Book": "5", "Death_Total": 42}, {"Book": "6", "Death_Total": 15}, {"Book": "7", "Death_Total": 3}, {"Book": "8", "Death_Total": 13}, {"Book": "9", "Death_Total": 0}, {"Book": "10", "Death_Total": 2}, {"Book": "11", "Death_Total": 39}, {"Book": "12", "Death_Total": 11}, {"Book": "13", "Death_Total": 18}, {"Book": "14", "Death_Total": 14}, {"Book": "15", "Death_Total": 18}, {"Book": "16", "Death_Total": 39}, {"Book": "17", "Death_Total": 8}, {"Book": "18", "Death_Total": 0}, {"Book": "19", "Death_Total": 0}, {"Book": "20", "Death_Total": 14}, {"Book": "21", "Death_Total": 9}, {"Book": "22", "Death_Total": 1}, {"Book": "23", "Death_Total": 0}, {"Book": "24", "Death_Total": 0}]);
+  const [dataArr, setDataArr] = useState ([{"Book": "1", "Direct_Attacks": 0}, {"Book": "2", "Direct_Attacks": 0}, {"Book": "3", "Direct_Attacks": 0}, {"Book": "4", "Direct_Attacks": 7}, {"Book": "5", "Direct_Attacks": 42}, {"Book": "6", "Direct_Attacks": 15}, {"Book": "7", "Direct_Attacks": 3}, {"Book": "8", "Direct_Attacks": 13}, {"Book": "9", "Direct_Attacks": 0}, {"Book": "10", "Direct_Attacks": 2}, {"Book": "11", "Direct_Attacks": 39}, {"Book": "12", "Direct_Attacks": 11}, {"Book": "13", "Direct_Attacks": 18}, {"Book": "14", "Direct_Attacks": 14}, {"Book": "15", "Direct_Attacks": 18}, {"Book": "16", "Direct_Attacks": 39}, {"Book": "17", "Direct_Attacks": 8}, {"Book": "18", "Direct_Attacks": 0}, {"Book": "19", "Direct_Attacks": 0}, {"Book": "20", "Direct_Attacks": 14}, {"Book": "21", "Direct_Attacks": 9}, {"Book": "22", "Direct_Attacks": 1}, {"Book": "23", "Direct_Attacks": 0}, {"Book": "24", "Direct_Attacks": 0}]);
+  const [selectedBooks, setSelectedBooks] = useState([1, 24]);
+  const [sliderValues, setSliderValues] = useState([1, 24]);
+
+  const setChartRange = () => {
+    setSelectedBooks(sliderValues);
+  }
+
+  const handleSliderChange = (sliderValues) => {setSliderValues(sliderValues)}
 
   return (
-    <LineChart width={800} height={400} data={(selectedBooks === [0, 24]) ? dataArr : dataArr.slice(selectedBooks[0], selectedBooks[1])} margin={{ top: 5, right: 20, bottom: 20, left: 0}}>
-      <Line type='monotone' dataKey='Death_Total' stroke='#8884d8' />
-      <XAxis dataKey="Book" label={{value: "Book", dy: 16}}/>
-      <YAxis dataKey="Death_Total" label={{value: "Death Events", angle: -90, dx: -10}}/>
-    </LineChart>
+    <div>
+      <h1 className="title">Deaths by Book</h1>
+      <div className="book-range-form">
+        <Range 
+          className="book-range" 
+          min={1} 
+          max={24} 
+          defaultValue={sliderValues}
+          allowCross='false' 
+          marks={{1: {label: '1'}, 2: {label: '2'}, 3: {label: '3'}, 4: {label: '4'}, 5: {label: '5'}, 6: {label: '6'}, 7: {label: '7'}, 8: {label: '8'}, 9: {label: '9'}, 10: {label: '10'}, 11: {label: '11'}, 12: {label: '12'}, 13: {label: '13'}, 14: {label: '14'}, 15: {label: '15'}, 16: {label: '16'}, 17: {label: '17'}, 18: {label: '18'}, 19: {label: '19'}, 20: {label: '20'}, 21: {label: '21'}, 22: {label: '22'}, 23: {label: '23'},  24: {label: '24'}}} 
+          onChange={handleSliderChange} 
+        />
+        <button className="book-range-submit" onClick={setChartRange}>Submit Range</button>
+      </div>
+      <div className='chart-container'>
+        <LineChart width={800} height={400} data={dataArr.slice(selectedBooks[0]-1, selectedBooks[1])} margin={{ top: 5, right: 20, bottom: 20, left: 0}}>
+          <Line type='monotone' dataKey='Direct_Attacks' stroke='#8884d8' />
+          <XAxis dataKey="Book" label={{value: "Book", dy: 16}}/>
+          <YAxis dataKey="Direct_Attacks" label={{value: "Death Events", angle: -90, dx: -10}}/>
+          <Tooltip />
+        </LineChart>
+      </div>
+    </div>
   )
 }
 
